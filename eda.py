7a7d7109ebad4b7020f15c9879e2d24c5b1bfce3
agjin9613 <agjin9613@gmail.com>
2024-05-03 15:55:54 +0900

    데이터 프레임 보기 수정

--- eda.py
+++ eda.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sb
-import matplotlib.pyplot as plt
 
 def run_eda():
     st.subheader('탐색적 데이터 분석')
@@ -19,34 +18,31 @@ def run_eda():
     elif choice_radio == radio_menu[1]:
         st.dataframe(df.describe())
 
-        # 최대/최소값 및 상관관계 분석
-        st.text('컬럼을 선택하면, 각 컬럼별 최대/최소 데이터를 보여드립니다.')
-        column_list = ['Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
-        choice_column = st.selectbox('컬럼을 선택하세요.', column_list)
-        
-        st.info(f'선택하신 {choice_column}의 최대 데이터는 다음과 같습니다.')
-        st.dataframe(df.loc[df[choice_column] == df[choice_column].max()])
-        
-        st.info(f'선택하신 {choice_column}의 최소 데이터는 다음과 같습니다.')
-        st.dataframe(df.loc[df[choice_column] == df[choice_column].min()])
+    # 최대/최소값 및 상관관계 분석
+    st.text('컬럼을 선택하면, 각 컬럼별 최대/최소 데이터를 보여드립니다.')
+    column_list = ['Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
+    choice_column = st.selectbox('컬럼을 선택하세요.', column_list)
+    
+    st.info(f'선택하신 {choice_column}의 최대 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].max()])
+    
+    st.info(f'선택하신 {choice_column}의 최소 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].min()])
 
-        st.subheader('상관관계 분석')
-        st.text('컬럼들을 2개 이상 선택하면, 컬럼들의 상관계수를 보여드립니다.')
+    st.subheader('상관관계 분석')
+    st.text('컬럼들을 2개 이상 선택하면, 컬럼들의 상관계수를 보여드립니다.')
 
-        corr_column_list = ['Age','Annual Salary', 'Credit Card Debt', 'Net Worth','Car Purchase Amount']
-        selected_columns = st.multiselect('컬럼을 선택하세요.', corr_column_list)
+    corr_column_list = ['Age','Annual Salary', 'Credit Card Debt', 'Net Worth','Car Purchase Amount']
+    selected_columns = st.multiselect('컬럼을 선택하세요.', corr_column_list)
 
-        # 2개 이상 선택했을때와 그렇지 않을때로 개발
-        if len(selected_columns) >= 2 : 
-            # 1. 페어플롯을 그린다.
-            # todo :
-            # 1. pairplot 을 다른 라이브러리 이용해서 하는 방법
-            # 2. pairplot 말고, 반복문으로 두 컬럼씩의 관계를 차트로 그리는방법
-            fig1 = sb.pairplot(data=df, vars=selected_columns)
-            st.pyplot(fig1)
-  
-            # 2. 상관계수 보여준다.
-            st.dataframe(df[selected_columns].corr())
-        
-        else : 
-            st.text('컬럼은 2개 이상 선택해야 합니다.')
+    # 2개 이상 선택했을때와 그렇지 않을때로 개발
+    if len(selected_columns) >= 2 : 
+        # 1. 페어플롯을 그린다.
+        fig1 = sb.pairplot(data=df, vars=selected_columns)
+        st.pyplot(fig1)
+
+        # 2. 상관계수 보여준다.
+        st.dataframe(df[selected_columns].corr())
+    
+    else : 
+        st.text('컬럼은 2개 이상 선택해야 합니다.')
